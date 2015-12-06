cce4bfd837864867df6c4456d94bf9d6b5522df4
novakov_alex <novakov.alex@gmail.com>
2015-12-06 13:26:02 -0500

    - zip assembler for homework submission

---
+++ src/main/scala-2.11/edu/princeton/alg2/HomeWorkAssembler.scala
@@ -0,0 +1,32 @@
+package edu.princeton.alg2
+
+import java.io.FileOutputStream
+import java.nio.file.Paths
+import java.util.zip.{ZipEntry, ZipOutputStream}
+
+import scala.io.Source
+
+/**
+  * @author Alexey Novakov
+  */
+object HomeWorkAssembler extends App {
+
+  val subPackageName = "week1"
+  val archiveName = "wordnet-testing.zip"
+  val srcRoot = "src/main/java/"
+
+  Paths.get(archiveName).toFile.delete
+
+  val path = Paths.get(srcRoot, getClass.getPackage.getName.replace(".", "/"), subPackageName)
+  val zip = new ZipOutputStream(new FileOutputStream(archiveName))
+
+  try {
+    for (file <- path.toFile.listFiles) {
+      val contentWithoutPackage = Source.fromFile(file).getLines().toList.tail.mkString("\n")
+      zip.putNextEntry(new ZipEntry(file.getName))
+      using(zip)(_.write(contentWithoutPackage.getBytes))
+    }
+  } finally zip.close
+
+  def using[A <: {def closeEntry() : Unit}, B](resource: A)(f: A => B): B = try f(resource) finally resource.closeEntry
+}
