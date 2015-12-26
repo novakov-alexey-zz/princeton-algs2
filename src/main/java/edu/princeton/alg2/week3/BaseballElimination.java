8413d8220ab8d102bd3f589998d792cc6c50d9f6
novakov_alex <novakov.alex@gmail.com>
2015-12-26 12:54:46 -0500

    - week3: trivial elimination

--- src/main/java/edu/princeton/alg2/week3/BaseballElimination.java
+++ src/main/java/edu/princeton/alg2/week3/BaseballElimination.java
@@ -2,83 +2,104 @@ package edu.princeton.alg2.week3;
 
 import edu.princeton.cs.algs4.In;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
  * @author Alexey Novakov
  */
 public class BaseballElimination {
-    private int[] w;
-    private int[] l;
-    private int[] r;
-    private int[][] g;
-    private Map<String, Integer> teamId;
+    private Map<String, Team> teams;
 
     // create a baseball division from given filename in format specified below
     public BaseballElimination(String filename) {
         In in = new In(filename);
 
         int n = in.readInt();
-        createStorage(n);
+        teams = new HashMap<>();
 
         for (int i = 0; !in.isEmpty() && i < n; i++) {
-            teamId.put(in.readString(), i);
-            w[i] = in.readInt();
-            l[i] = in.readInt();
-            r[i] = in.readInt();
+            Team team = new Team();
+            teams.put(in.readString(), team);
 
+            team.id = i;
+            team.w = in.readInt();
+            team.l = in.readInt();
+            team.r = in.readInt();
+            team.g = new int[n];
             for (int j = 0; j < n; j++) {
-                g[i][j] = in.readInt();
+                team.g[j] = in.readInt();
             }
         }
+
+        elimination();
     }
 
-    private void createStorage(int n) {
-        w = new int[n];
-        l = new int[n];
-        r = new int[n];
-        g = new int[n][n];
-        teamId = new HashMap<>();
+    private void elimination() {
+        //Trivial elimination
+        for (Team team : teams.values()) {
+            teams.keySet().stream()
+                    .filter(anotherTeam -> teams.get(anotherTeam).w >= team.w + team.r)
+                    .findFirst()
+                    .ifPresent(anotherTeam -> {
+                        team.eliminated = true;
+                        team.eliminationCertificate.add(anotherTeam);
+                    });
+        }
+
+        //Nontrivial elimination
+        //TODO
     }
 
     // number of teams
     public int numberOfTeams() {
-        return teamId.size();
+        return teams.size();
     }
 
     // all teams
     public Iterable<String> teams() {
-        return teamId.keySet();
+        return teams.keySet();
     }
 
     // number of wins for given team
     public int wins(String team) {
-        return w[teamId.get(team)];
+        return teams.get(team).w;
     }
 
     // number of losses for given team
     public int losses(String team) {
-        return l[teamId.get(team)];
+        return teams.get(team).l;
     }
 
     // number of remaining games for given team
     public int remaining(String team) {
-        return r[teamId.get(team)];
+        return teams.get(team).r;
     }
 
     // number of remaining games between team1 and team2
     public int against(String team1, String team2) {
-        return g[teamId.get(team1)][teamId.get(team2)];
+        return teams.get(team1).g[teams.get(team2).id];
     }
 
     // is given team eliminated?
     public boolean isEliminated(String team) {
-        return false;
+        return teams.get(team).eliminated;
     }
 
     // subset R of teams that eliminates given team; null if not eliminated
     public Iterable<String> certificateOfElimination(String team) {
-        return teamId.keySet();
+        return teams.get(team).eliminationCertificate;
+    }
+
+    private static class Team {
+        private int id;
+        private int w;
+        private int l;
+        private int r;
+        private int[] g;
+        private boolean eliminated;
+        private List<String> eliminationCertificate = new ArrayList<>();
     }
 }
--- src/test/scala-2.11/edu/princeton/alg2/week3/BaseballEliminationTest.scala
+++ src/test/scala-2.11/edu/princeton/alg2/week3/BaseballEliminationTest.scala
@@ -3,21 +3,27 @@ package edu.princeton.alg2.week3
 import edu.princeton.cs.algs4.StdOut
 import org.scalatest.{FlatSpec, Matchers}
 
+import scala.collection.JavaConversions._
+
 /**
   * @author Alexey Novakov
   */
 class BaseballEliminationTest extends FlatSpec with Matchers {
   behavior of "BaseballElimination"
 
+  def fixture = {
+    new {
+      val fileName = "teams4.txt"
+      val division = new BaseballElimination(s"baseball/$fileName")
+    }
+  }
+
   it should "load data from file" in {
-    //given
-    val fileName = "teams4.txt"
     //when
-    val division = new BaseballElimination(s"baseball/$fileName")
+    val division = fixture.division
     //then
     division.numberOfTeams shouldBe 4
     //then
-    import scala.collection.JavaConversions._
     division.teams.toSet shouldBe Set("New_York", "Atlanta", "Philadelphia", "Montreal")
     //then
     division.wins("New_York") shouldBe 78
@@ -29,16 +35,23 @@ class BaseballEliminationTest extends FlatSpec with Matchers {
     division.against("Atlanta", "Philadelphia") shouldBe 1
   }
 
+  it should "return eliminated" in {
+    fixture.division.isEliminated("Montreal") shouldBe true
+  }
+
+  it should "return certificate of elimination" in {
+    fixture.division.certificateOfElimination("Montreal").toSet shouldBe Set("Atlanta")
+  }
+
   def printEliminatedTeams(fileName: String) {
     val division: BaseballElimination = new BaseballElimination(fileName)
-    import scala.collection.JavaConversions._
 
     division.teams.foreach {
       case t if division.isEliminated(t) =>
         StdOut.print(s"$t is eliminated by the subset R = { ")
         division.certificateOfElimination(t).foreach(et => StdOut.print(s"$et "))
         StdOut.println("}")
-      case t =>  StdOut.println(s"$t is not eliminated")
+      case t => StdOut.println(s"$t is not eliminated")
     }
   }
 }
